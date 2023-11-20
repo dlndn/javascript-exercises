242a2cbb0438299493e346921cdcd44129c34282
dlndn <65935030+dlndn@users.noreply.github.com>
2023-11-19 17:04:01 -0800

    solved 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let characters = string.split("");
+    let reversed = "";
 
+    for (let i=string.length-1; i>=0; i--) {
+        reversed += characters[i];
+    }
+
+    return reversed;
 };
 
 // Do not edit below this line
