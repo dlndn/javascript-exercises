0a073e08f1de8ef3a6ca83bffe931a4e2b955ed7
dlndn <65935030+dlndn@users.noreply.github.com>
2023-11-19 18:15:16 -0800

    solved 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    for (let i=1; i<arguments.length; i++) {
+        removeElement(array, arguments[i]);
+    }
+    return array;
 };
+
+const removeElement = function(array, element) {
+    const index = array.indexOf(element);
+    if (index > -1) { 
+        array.splice(index, 1);
+    }
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
